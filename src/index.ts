e49fc82e78c758ab57cd959db054c30e06f26e9e
Velczer <32433383+Velczer@users.noreply.github.com>
2026-05-23 19:43:19 +0200

    app.listen check

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import { startBot } from './bot/index.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`API działa na porcie ${PORT}`);
 });
 
