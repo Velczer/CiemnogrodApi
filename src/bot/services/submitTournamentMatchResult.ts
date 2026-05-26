8debcf9d278fde848d50afdd7a5da66722a0606c
Velczer <32433383+Velczer@users.noreply.github.com>
2026-05-26 21:09:55 +0200

    fix thirdPlace advance

--- src/bot/services/submitTournamentMatchResult.ts
+++ src/bot/services/submitTournamentMatchResult.ts
@@ -62,6 +62,17 @@ export async function submitTournamentMatchResult(
           name: match.player2Name,
         };
 
+  const loser =
+    score1 > score2
+      ? {
+          id: match.player2Id,
+          name: match.player2Name,
+        }
+      : {
+          id: match.player1Id,
+          name: match.player1Name,
+        };
+
   await prisma.tournamentMatch.update({
     where: {
       id: match.id,
@@ -75,8 +86,102 @@ export async function submitTournamentMatchResult(
     },
   });
 
-  // finał
-  if (!match.nextMatchNumber || !match.nextSlot) {
+  // WRZUĆ ZWYCIĘZCĘ DO NASTĘPNEGO MECZU
+  if (match.nextMatchNumber && match.nextSlot) {
+    const nextMatch = await prisma.tournamentMatch.findFirst({
+      where: {
+        tournamentId: tournament.id,
+        matchNumber: match.nextMatchNumber,
+      },
+    });
+
+    if (!nextMatch) {
+      throw new Error('Nie znaleziono następnego meczu');
+    }
+
+    await prisma.tournamentMatch.update({
+      where: {
+        id: nextMatch.id,
+      },
+      data:
+        match.nextSlot === 1
+          ? {
+              player1Id: winner.id,
+              player1Name: winner.name,
+            }
+          : {
+              player2Id: winner.id,
+              player2Name: winner.name,
+            },
+    });
+
+    const updatedNextMatch = await prisma.tournamentMatch.findFirst({
+      where: {
+        id: nextMatch.id,
+      },
+    });
+
+    if (updatedNextMatch?.player1Id && updatedNextMatch?.player2Id) {
+      await prisma.tournamentMatch.update({
+        where: {
+          id: nextMatch.id,
+        },
+        data: {
+          status: 'live',
+        },
+      });
+    }
+  }
+
+  // WRZUĆ PRZEGRANEGO DO WALKI O 3 MIEJSCE
+  if (match.loserNextMatchNumber && match.loserNextSlot) {
+    const loserNextMatch = await prisma.tournamentMatch.findFirst({
+      where: {
+        tournamentId: tournament.id,
+        matchNumber: match.loserNextMatchNumber,
+      },
+    });
+
+    if (!loserNextMatch) {
+      throw new Error('Nie znaleziono meczu o 3 miejsce');
+    }
+
+    await prisma.tournamentMatch.update({
+      where: {
+        id: loserNextMatch.id,
+      },
+      data:
+        match.loserNextSlot === 1
+          ? {
+              player1Id: loser.id,
+              player1Name: loser.name,
+            }
+          : {
+              player2Id: loser.id,
+              player2Name: loser.name,
+            },
+    });
+
+    const updatedLoserMatch = await prisma.tournamentMatch.findFirst({
+      where: {
+        id: loserNextMatch.id,
+      },
+    });
+
+    if (updatedLoserMatch?.player1Id && updatedLoserMatch?.player2Id) {
+      await prisma.tournamentMatch.update({
+        where: {
+          id: loserNextMatch.id,
+        },
+        data: {
+          status: 'live',
+        },
+      });
+    }
+  }
+
+  // KONIEC TURNIEJU
+  if (match.round === 'Final' && !match.nextMatchNumber) {
     await prisma.tournament.update({
       where: {
         id: tournament.id,
@@ -90,50 +195,6 @@ export async function submitTournamentMatchResult(
       winner,
       tournamentCompleted: true,
     };
-  }
-
-  const nextMatch = await prisma.tournamentMatch.findFirst({
-    where: {
-      tournamentId: tournament.id,
-      matchNumber: match.nextMatchNumber,
-    },
-  });
-
-  if (!nextMatch) {
-    throw new Error('Nie znaleziono następnego meczu');
-  }
-
-  await prisma.tournamentMatch.update({
-    where: {
-      id: nextMatch.id,
-    },
-    data:
-      match.nextSlot === 1
-        ? {
-            player1Id: winner.id,
-            player1Name: winner.name,
-          }
-        : {
-            player2Id: winner.id,
-            player2Name: winner.name,
-          },
-  });
-
-  const updatedNextMatch = await prisma.tournamentMatch.findFirst({
-    where: {
-      id: nextMatch.id,
-    },
-  });
-
-  if (updatedNextMatch?.player1Id && updatedNextMatch?.player2Id) {
-    await prisma.tournamentMatch.update({
-      where: {
-        id: nextMatch.id,
-      },
-      data: {
-        status: 'live',
-      },
-    });
   }
 
   return {
