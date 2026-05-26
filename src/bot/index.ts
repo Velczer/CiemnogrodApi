ac3d2ccfb189e4d18e4a6c6a1fd23ca687ece2d3
Velczer <32433383+Velczer@users.noreply.github.com>
2026-05-26 19:04:52 +0200

    add import tournament handler

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -2,9 +2,14 @@ import dotenv from 'dotenv';
 import { client } from './client.js';
 import './interactions/buttons.js';
 import './handlers/heroesHandler.js';
+import './handlers/startTournamentHandler.js';
 
 dotenv.config();
 
 export function startBot() {
   client.login(process.env.DISCORD_TOKEN);
+
+  client.once('ready', () => {
+    console.log(`Bot zalogowany jako ${client.user?.tag}`);
+  });
 }
